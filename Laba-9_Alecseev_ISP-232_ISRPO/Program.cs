e53b236ea17b3e0c73d630adbc82c54298671bf6
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-22 13:14:40 +0300

    Добавьте файлы проекта.

---
+++ Laba-9_Alecseev_ISP-232_ISRPO/Program.cs
@@ -0,0 +1,14 @@
+﻿using MathLibrary;
+namespace Laba_9_Alecseev_ISP_232_ISRPO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathTools a = new MathTools();
+            Console.WriteLine(a.Add(5, 10));
+            Console.WriteLine(a.Multiply(5, 10));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MathLibrary/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MathLibrary
+{
+    public class MathTools
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
