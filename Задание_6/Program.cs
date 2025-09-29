051fffa7350052ed709604fd4a5618b9d1b70f49
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-29 14:35:33 +0300

    Задание 6

---
+++ Задание_6/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Задание_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SmartLight light = new SmartLight();
+            light.OnStateChanged += lightisOn;
+            light.OnLightChanged += lightCondition;
+            light.IsOn = true;
+            light.SetBrightness(90);
+
+            light.Toggle();
+            light.Toggle();
+
+            string json = light.ToJson();
+            Console.WriteLine($"JSON: {json}");
+
+            SmartLight l = SmartLight.FromJson(json);
+            Console.WriteLine($"{l.IsOn}:{l.Brightness}");
+        }
+        static void lightisOn(bool isOn)
+        {
+            Console.WriteLine(isOn ? "Свет вкл" : "Свет выкл");
+        }
+        static void lightCondition(int brightness)
+        {
+            Console.WriteLine($"Яркость света {brightness}");
+        }
+    }
+}
---
+++ Задание_6/SmartLight.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Задание_6
+{
+    public delegate void ChangedHandler(bool condition);
+    public delegate void LightChangedHandler(int condition);
+    internal class SmartLight
+    {
+        public event ChangedHandler OnStateChanged;
+        public event LightChangedHandler OnLightChanged;
+        public bool IsOn { get; set; }
+        public int Brightness { get; set; }
+
+        public void Toggle()
+        {
+            IsOn = !IsOn;
+            OnStateChanged?.Invoke(IsOn);
+        }
+
+        public void SetBrightness(int brightness)
+        {
+            Brightness = brightness;
+            OnLightChanged?.Invoke(Brightness);
+        }
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+        public static SmartLight FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<SmartLight>(json);
+        }
+
+    }
+}
