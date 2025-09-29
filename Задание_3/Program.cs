efe283c3c67062e56322944016575d160b08d614
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-29 12:59:03 +0300

    Задание 3

---
+++ Задание_3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Задание_3
+{
+    internal class Program
+    {
+        public delegate void Logger(string message);
+        static void Main(string[] args)
+        {
+            
+            Logger deleg = LogToConsole;
+            deleg("hello world");
+            Console.ReadKey();
+        }
+        static void LogToConsole(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
