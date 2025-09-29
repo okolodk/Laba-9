7f29d0795408465985b4208b8fd041dcb216dc32
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-29 12:45:51 +0300

    Задание 2

---
+++ Задание_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using HtmlAgilityPack;
+
+namespace Задание_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://example.com");
+
+            // Ищем заголовок
+            HtmlNode titleNode = doc.DocumentNode.SelectSingleNode("//title");
+
+            if (titleNode != null)
+            {
+                Console.WriteLine($"Заголовок: {titleNode.InnerText}");
+            }
+            else
+            {
+                Console.WriteLine("Заголовок не найден!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
