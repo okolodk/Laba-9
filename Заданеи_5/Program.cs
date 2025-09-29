86692116dd578b9639bf664b2181f53cfedac6c2
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-29 13:51:13 +0300

    Задание 5

---
+++ Заданеи_5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Заданеи_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintLength("123");
+            PrintLength(null);
+        }
+
+        static void PrintLength(string? input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Cтрока отсутствует");
+            }
+            else
+            {
+                Console.WriteLine($"Длина строки {input.Length}");
+            }
+        }
+    }
+}
