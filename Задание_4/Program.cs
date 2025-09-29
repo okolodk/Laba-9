9984d4869d614a4b87e9ada1789e8015fb8de9b4
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-29 13:41:41 +0300

    Задание 4

---
+++ Задание_4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Задание_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thermometer thermometer = new Thermometer();
+
+            thermometer.TemperatureTooHigh += OnTemperatureTooHigh;
+
+            thermometer.Measure(25);
+            thermometer.Measure(105);
+            Console.ReadKey();
+        }
+        static void OnTemperatureTooHigh(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    
+
+}
---
+++ Задание_4/Thermometer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public delegate void TemperatureCount(string mas);
+public class Thermometer
+{
+    public event TemperatureCount TemperatureTooHigh;
+
+    public void Measure(int value)
+    {
+        Console.WriteLine($"Tемпература = {value}");
+
+        if (value > 100)
+        {
+            TemperatureTooHigh?.Invoke($"Tемпература превышена: {value}");
+        }
+    }
+}
